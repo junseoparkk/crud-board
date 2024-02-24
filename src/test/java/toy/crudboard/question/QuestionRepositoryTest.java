ab476581565582c9bfd35bd209e5c3d52c3d3066
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-25 04:10:38 +0900

    feat: 질문 제목 및 내용으로 검색하는 기능 구현

--- src/test/java/toy/crudboard/question/QuestionRepositoryTest.java
+++ src/test/java/toy/crudboard/question/QuestionRepositoryTest.java
@@ -23,9 +23,9 @@ public class QuestionRepositoryTest {
     @Transactional
     @DisplayName("질문 저장 테스트")
     @Test
-    void save() {
-        Question question = new Question();
+    void saveTest() {
         //given
+        Question question = new Question();
         question.setSubject("test_subject");
         question.setContent("test_content");
         question.setAnswers(new ArrayList<Answer>());
@@ -43,9 +43,45 @@ public class QuestionRepositoryTest {
     @Transactional
     @DisplayName("존재하지 않는 질문 아이디로 검색시 예외 발생 테스트")
     @Test
-    void exceptionWithNotExistQuestion() {
+    void exceptionWithNotExistQuestionTest() {
         //when, then
         assertThrows(IllegalArgumentException.class, () ->
                 questionRepository.findById(1L).orElseThrow(IllegalArgumentException::new));
+    }
+
+    @DisplayName("질문 제목으로 검색 테스트")
+    @Test
+    void findBySubjectTest() {
+        //given
+        Question question = new Question();
+        question.setSubject("test_subject");
+        question.setContent("test_content");
+        question.setAnswers(new ArrayList<Answer>());
+        question.setCreateDate(LocalDateTime.now());
+
+        //when
+        questionRepository.save(question);
+
+        //then
+        Question findQuestion = questionRepository.findBySubject(question.getSubject());
+        assertThat(question.getId()).isEqualTo(findQuestion.getId());
+    }
+
+    @DisplayName("질문 제목 및 글 내용으로 검색 테스트")
+    @Test
+    void findBySubjectAndContentTest() {
+        //given
+        Question question = new Question();
+        question.setSubject("test_subject");
+        question.setContent("test_content");
+        question.setAnswers(new ArrayList<Answer>());
+        question.setCreateDate(LocalDateTime.now());
+
+        //when
+        questionRepository.save(question);
+
+        //then
+        Question findQuestion = questionRepository.findBySubjectAndContent(question.getSubject(), question.getContent());
+        assertThat(question.getId()).isEqualTo(findQuestion.getId());
     }
 }
