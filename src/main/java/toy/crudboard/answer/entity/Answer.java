2e44851b9cc32f3d8f0b9d77909d48c127ef95ed
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-25 03:42:22 +0900

    feat: Answer 엔티티 구현 및 연관관계 매핑

--- src/main/java/toy/crudboard/answer/entity/Answer.java
+++ src/main/java/toy/crudboard/answer/entity/Answer.java
@@ -1,34 +1,31 @@
-package toy.crudboard.question.entity;
+package toy.crudboard.answer.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-import toy.crudboard.answer.entity.Answer;
+import toy.crudboard.question.entity.Question;
 
 @Entity
-@Getter @Setter
-@Table(name = "question")
-public class Question {
+@Getter
+@Setter
+@Table(name = "answer")
+public class Answer {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 20)
-    private String subject;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Question question;
 
     @Column(columnDefinition = "TEXT")
     private String content;
-
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answers;
 
     @Column(name = "create_date")
     private LocalDateTime createDate;
