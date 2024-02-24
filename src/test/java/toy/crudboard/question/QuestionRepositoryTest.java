c0e243f697644fdfe99e95954b530ad8cc791f23
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-25 04:04:01 +0900

    test: 질문 레포지토리 저장 테스트 구현

---
+++ src/test/java/toy/crudboard/question/QuestionRepositoryTest.java
@@ -0,0 +1,51 @@
+package toy.crudboard.question;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import toy.crudboard.answer.entity.Answer;
+import toy.crudboard.question.entity.Question;
+import toy.crudboard.question.repository.QuestionRepository;
+
+@SpringBootTest
+public class QuestionRepositoryTest {
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @Transactional
+    @DisplayName("질문 저장 테스트")
+    @Test
+    void save() {
+        Question question = new Question();
+        //given
+        question.setSubject("test_subject");
+        question.setContent("test_content");
+        question.setAnswers(new ArrayList<Answer>());
+        question.setCreateDate(LocalDateTime.now());
+
+        //when
+        questionRepository.save(question);
+
+        //then
+        Question findQuestion = questionRepository.findById(question.getId())
+                .orElseThrow(() -> new IllegalArgumentException("no such question"));
+        assertThat(question.getId()).isEqualTo(findQuestion.getId());
+    }
+
+    @Transactional
+    @DisplayName("존재하지 않는 질문 아이디로 검색시 예외 발생 테스트")
+    @Test
+    void exceptionWithNotExistQuestion() {
+        //when, then
+        assertThrows(IllegalArgumentException.class, () ->
+                questionRepository.findById(1L).orElseThrow(IllegalArgumentException::new));
+    }
+}
