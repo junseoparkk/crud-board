42b8b491cc70ae045b3711cf60333578ca162469
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-25 03:35:48 +0900

    initial commit

---
+++ src/test/java/toy/crudboard/CrudBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.crudboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
