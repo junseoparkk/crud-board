f9f48e26bc7f47eadc817b699d79b3a54d3eab54
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-26 01:01:01 +0900

    refactor: 패스워드 인코더 DI 적용

--- src/main/java/toy/crudboard/user/service/UserServiceImpl.java
+++ src/main/java/toy/crudboard/user/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package toy.crudboard.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import toy.crudboard.user.entity.User;
 import toy.crudboard.user.repository.UserRepository;
@@ -10,13 +11,13 @@ import toy.crudboard.user.repository.UserRepository;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User signup(String name, String email, String password) {
         User user = new User();
         user.setName(name);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
         userRepository.save(user);
         return user;
