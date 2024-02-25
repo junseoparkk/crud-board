60326e0a5c695d529945ea1bd356187e7819a630
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-25 18:35:10 +0900

    refactor: QuestionController URL 프리픽스 설정

--- src/main/java/toy/crudboard/question/controller/QuestionController.java
+++ src/main/java/toy/crudboard/question/controller/QuestionController.java
@@ -6,22 +6,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import toy.crudboard.question.entity.Question;
 import toy.crudboard.question.service.QuestionService;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questions = questionService.findAll();
         model.addAttribute("questions", questions);
         return "question_list";
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("detail/{id}")
     public String detail(Model model, @PathVariable(name = "id") Long id) {
         Question question = questionService.findById(id);
         model.addAttribute(question);
