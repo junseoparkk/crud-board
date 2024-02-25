84be39fa4aa3f8af20fbe9d8a81602b8cd1e84e6
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-26 01:29:19 +0900

    feat: 이미 등록된 사용자인지 검증하는 기능 구현

--- src/main/java/toy/crudboard/user/controller/UserController.java
+++ src/main/java/toy/crudboard/user/controller/UserController.java
@@ -3,6 +3,8 @@ package toy.crudboard.user.controller;
 import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import toy.crudboard.user.entity.UserSignupForm;
 import toy.crudboard.user.service.UserService;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/user")
@@ -37,7 +40,18 @@ public class UserController {
             return "signup_form";
         }
 
-        userService.signup(userSignupForm.getName(), userSignupForm.getEmail(), userSignupForm.getPassword());
+        try {
+            userService.signup(userSignupForm.getName(), userSignupForm.getEmail(), userSignupForm.getPassword());
+        } catch (DataIntegrityViolationException e) {
+            log.info(e.getMessage());
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
+
         return "redirect:/";
     }
 }
