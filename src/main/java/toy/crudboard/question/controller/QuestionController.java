cf9b1fb8a28d459c4c26ce5ca4410ef988ce9f18
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-25 10:55:23 +0900

    feat: 질문 컨트롤러 구현, 질문 목록 페이지 url매핑

--- src/main/java/toy/crudboard/question/controller/QuestionController.java
+++ src/main/java/toy/crudboard/question/controller/QuestionController.java
@@ -1,2 +1,21 @@
-package toy.crudboard.question.controller;public class QuestionController {
+package toy.crudboard.question.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import toy.crudboard.question.entity.Question;
+import toy.crudboard.question.service.QuestionService;
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+    private final QuestionService questionService;
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questions = questionService.findAll();
+        model.addAttribute("questions", questions);
+        return "question_list";
+    }
 }
