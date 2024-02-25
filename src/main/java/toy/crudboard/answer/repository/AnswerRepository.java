45d920492f46c5bf9231c1596012b78bde70e1f7
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-25 10:40:09 +0900

    refactor: @Repository 어노테이션 추가

--- src/main/java/toy/crudboard/answer/repository/AnswerRepository.java
+++ src/main/java/toy/crudboard/answer/repository/AnswerRepository.java
@@ -1,7 +1,9 @@
 package toy.crudboard.answer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import toy.crudboard.answer.entity.Answer;
 
+@Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 }
--- src/main/java/toy/crudboard/question/repository/QuestionRepository.java
+++ src/main/java/toy/crudboard/question/repository/QuestionRepository.java
@@ -1,8 +1,10 @@
 package toy.crudboard.question.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import toy.crudboard.question.entity.Question;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
